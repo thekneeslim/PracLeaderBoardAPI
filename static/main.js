85a6e985851fa8f59f641b66d01ce018f5a5efcc
Denise Lim <denise.lim.sh@gmail.com>
2016-09-13 21:18:56 +0800

    IT WORKS

--- static/main.js
+++ static/main.js
@@ -16,12 +16,36 @@ document.addEventListener("DOMContentLoaded", function() {
 
       var tr;
       for (var i = 0; i < data.length; i++) {
+
+
           tr = $('<tr/>');
           tr.append("<td>" + data[i].name + "</td>");
           tr.append("<td>" + data[i].score + "</td>");
           tr.append("<td>" + data[i].initial + "</td>");
+          tr.append("<td>" + "<button type='button' class='removebutton' title='Remove this row'>X</button>" + "</td>");
           $('table').append(tr);
       }
     };
   })
+});
+
+var i = 1;
+$("#addbutton").click(function () {
+    $("table tr:first").clone().find("input").each(function () {
+        $(this).val('').attr({
+            'id': function (_, id) {
+                return id + i
+            },
+                'name': function (_, name) {
+                return name + i
+            },
+                'value': ''
+        });
+    }).end().appendTo("table");
+    i++;
+});
+
+$(document).on('click', 'button.removebutton', function () {
+    $(this).closest('tr').remove();
+    return false;
 });
